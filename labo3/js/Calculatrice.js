d0664ceeafc681e6ecaa4c7ff6859e21e5ec8c72
Nadia <nadia.chicoine@hotmail.com>
2017-01-26 19:21:48 -0500

    calculatrice is progressing

--- labo3/js/Calculatrice.js
+++ labo3/js/Calculatrice.js
@@ -2,6 +2,41 @@
  * Created by Nadia and Raphael on 2017-01-26.
  */
 
- function Calculatrice(firstNumber) {
-     this.number = firstNumber;
+ function Calculatrice(number) {
+     this.number = number;
+     this.add = function(number) {
+         this.number += number;
+         return this;
+     }
+     this.sub = function(number) {
+         this.number -= number;
+         return this;
+     }
+     this.multiply = function(number) {
+         this.number *= number;
+         return this;
+     }
+     this.divide = function(number) {
+         this.number /= number;
+         return this;
+     }
+     this.sin = function() {
+         this.number = Math.sin(this.number);
+         return this;
+     }
+     this.cos = function() {
+         this.number = Math.sin(this.number);
+         return this;
+     }
+     this.factorial = function() {
+         factorial(this.number);
+         return this;
+     }
+     function factorial(number) {
+         if (number > 1){
+             this.number *= (number - 1);
+             factorial(number - 1);
+         }
+     }
  };
+
