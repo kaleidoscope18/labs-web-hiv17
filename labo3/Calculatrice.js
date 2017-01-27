ecd6d4446420b7823d485f6d3bb4395020ad1ad2
Nadia <nadia.chicoine@hotmail.com>
2017-01-26 19:02:15 -0500

    MAJ

--- labo3/Calculatrice.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Nadia on 2017-01-26.
- */
---
+++ labo3/js/Calculatrice.js
@@ -0,0 +1,7 @@
+ /**
+ * Created by Nadia and Raphael on 2017-01-26.
+ */
+
+ function Calculatrice(firstNumber) {
+     this.number = firstNumber;
+ };
