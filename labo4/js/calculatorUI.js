0beac9ec1193a44cbaf595b0cccd5dc5fb624a5c
raphaelmarcouxt <raphaelmarcouxt@hotmail.ca>
2017-02-02 21:19:12 -0500

    Géoloc avec bouton

--- labo4/js/calculatorUI.js
+++ labo4/js/calculatorUI.js
@@ -2,19 +2,23 @@
  * Created by Nadia on 2017-02-02.
  */
 
-var strToCalculate = "";
+
+var calculator = new Calculator();
+var value = 0;
+var operator = calculator.value;
+var displayString = '';
 
 var generateUI = function() {
     generateButtons();
 }
 
 var generateButtons = function() {
-    var symbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/"];
+    var symbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "="];
     for(var i = 0; i < 10; i++){
-        createButton(symbols[i], btnFct(symbols[i]));
+        createButton(symbols[i], onNumberButton(symbols[i]));
     }
-    for(var i = 10; i < symbols.length; i++){
-        createButton(symbols[i], btnFct(symbols[i]));
+    for(i = 10; i < symbols.length; i++){
+        createButton(symbols[i], onOperatorButton(symbols[i]));
     }
 }
 
@@ -24,9 +28,26 @@ var createButton = function(symbol, callback){
     btn.click(callback);
 }
 
-var btnFct = function(symbol) {
+var onNumberButton = function(symbol) {
     return function() {
-        strToCalculate += symbol;
+        value += symbol.toString();
+    }
+}
+
+var onOperatorButton = function(symbol) {
+    return function() {
+        operator(value);
+        if(symbol == '+'){
+            operator = calculator.add;
+        }
+        else if(symbol == '-'){
+            operator = calculator.subtract;
+        }
+        else if(symbol == '='){
+            console.log(calculator.equals());
+            operator = calculator.value;
+        }
+        value = 0;
     }
 }
 
