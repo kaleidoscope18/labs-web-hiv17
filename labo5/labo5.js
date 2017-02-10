61eadcdedcf9f09463a71cc00c59e082a7de38a6
Raphael <raphaelmarcouxt@hotmail.ca>
2017-02-09 19:59:46 -0500

    Ajouté post

--- labo5/labo5.js
+++ labo5/labo5.js
@@ -4,20 +4,34 @@
 
 var currentId = 0;
 
+var myurl = "http://localhost:5000/tasks";
+
 var addTask = function(textTask, idTask) {
     $(".taskList").append($('<ul id="'+idTask+'">'+textTask+'</ul>'));
 }
 
-var updateListMock = function() {
-    addTask("blabla", "lol");
-    console.log("bla");
+var postTask = function(){
+    var text = $("#taskInputArea").val();
+    $.ajax({
+        url : myurl,
+        type : 'POST',
+        data : {'task': text},
+        contentType: 'application/json'
+    })
+        .done(function(data) {
+            updateList();
+        })
+        .fail(function(jqXHR, textStatus) {
+            console.log("error");
+        });
+
 }
 
 var updateList = function() {
-    // $(".taskList").
+    console.log('success');
 }
 
 $(document).ready( function() {
-    $("#taskInputBtn").click(updateListMock);
+    $("#taskInputBtn").click(postTask);
     $("#refreshBtn").click(updateList);
 })
