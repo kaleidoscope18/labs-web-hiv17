944cc5eb9a69f50350e75f4bbb3fed9413029271
Nadia <nadia.chicoine@hotmail.com>
2017-02-09 19:18:28 -0500

    just little stuff

--- labo5/labo5.js
+++ labo5/labo5.js
@@ -1,3 +1,23 @@
 /**
  * Created by Nadia on 2017-02-09.
  */
+
+var currentId = 0;
+
+var addTask = function(textTask, idTask) {
+    $(".taskList").append($('<ul id="'+idTask+'">'+textTask+'</ul>'));
+}
+
+var updateListMock = function() {
+    addTask("blabla", "lol");
+    console.log("bla");
+}
+
+var updateList = function() {
+    // $(".taskList").
+}
+
+$(document).ready( function() {
+    $("#taskInputBtn").click(updateListMock);
+    $("#refreshBtn").click(updateList);
+})
